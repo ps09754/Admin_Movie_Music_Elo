6f3da119c7b9e69a504f9f63c343abd42e1907ce
baalzbarbarian <nguyenduchai.19021992@gmail.com>
2020-10-15 10:03:33 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/ps09754/Admin_Movie_Music_Elo""
    This reverts commit fc0a5d20a6a7bd66376e00dd204ef9d41a0be034.

--- Models/User.js
+++ Models/User.js
@@ -1,25 +1,21 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const user = new schema({
-    create_at:Date,
-    update_at:Date,
-    delete_at:Date,
-    token_google: {
-        idToken: {type: String, unique: true, require: true},
-        user: {
-            email: {type: String, unique: true, require: true},
-            photo: String, 
-            name: String 
-        }
+var user = new mongoose.Schema({
+    login_at:Date,
+    google: {
+        name: { type: String },
+        gmail: { type: String },
+        photo: { type: String },
+        token: { type: String }
     },
-    token_facebook: {
-        idToken: {type: String, unique: true, require: true},
-        user:{
-            name:String,
-            photo:String,
-        }
-     }
+    facebook: {
+        id: { type: String },
+        token: { type: String },
+        gmail: { type: String },
+        name: { type: String },
+        photo: { type: String }
+    }
 });
 
 module.exports = mongoose.model("tbl_user", user);
--- Routes/User.route.js
+++ Routes/User.route.js
@@ -2,9 +2,12 @@ const { request } = require('express');
 
 module.exports = function (app) {
     const User = require('../Controllers/User.controller')
-    app.route('/v1/user/login')
+    app.route('/v1/user/login/:type')
         .post(User._addUser)
     
     app.route('/v3/user/getUser/:id')
         .get(User._getDataUser_by_id)
+
+    app.route('/v4/user/async/:_id/:type')
+        .put(User._asyncUser)
 };
